4294c812d99f7eeda4eed4bce44e4d3be69c212d
Aviator Yonathaniel <aviatoryona67@gmail.com>
2020-11-05 08:47:43 +0300

    Initial commit

---
+++ app/src/main/java/com/aviatordev/mutablelivedataexamplemvvm/MainActivity.java
@@ -0,0 +1,84 @@
+package com.aviatordev.mutablelivedataexamplemvvm;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.aviatordev.mutablelivedataexamplemvvm.databinding.ActivityMainBinding;
+import com.aviatordev.mutablelivedataexamplemvvm.models.LoginUser;
+import com.aviatordev.mutablelivedataexamplemvvm.viewmodel.LoginViewModel;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LoginViewModel loginViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
+        binding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
+        setSupportActionBar(binding.toolbar);
+        binding.setLifecycleOwner(this);
+
+        binding.setLoginViewModel(loginViewModel);
+
+        loginViewModel.getUser().observe(this, new Observer<LoginUser>() {
+            @Override
+            public void onChanged(@Nullable LoginUser loginUser) {
+
+                if (TextUtils.isEmpty(Objects.requireNonNull(loginUser).getStrEmailAddress())) {
+                    binding.txtEmailAddress.setError("Enter an E-Mail Address");
+                    binding.txtEmailAddress.requestFocus();
+                }
+                else if (!loginUser.isEmailValid()) {
+                    binding.txtEmailAddress.setError("Enter a Valid E-mail Address");
+                    binding.txtEmailAddress.requestFocus();
+                }
+                else if (TextUtils.isEmpty(Objects.requireNonNull(loginUser).getStrPassword())) {
+                    binding.txtPassword.setError("Enter a Password");
+                    binding.txtPassword.requestFocus();
+                }
+                else if (!loginUser.isPasswordLengthGreaterThan5()) {
+                    binding.txtPassword.setError("Enter at least 6 Digit password");
+                    binding.txtPassword.requestFocus();
+                }
+                else {
+                    binding.lblEmailAnswer.setText(loginUser.getStrEmailAddress());
+                    binding.lblPasswordAnswer.setText(loginUser.getStrPassword());
+                }
+
+            }
+        });
+    }
+@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
